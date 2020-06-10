9e37345cf99c210ab374879be46de80964c57b92
preethu08052k <preethivardhanega@gmail.com>
2020-06-10 15:58:14 +0530

    heroku testing

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
-# from flask_restful import Api
-# from resources.emp import Emp
+from flask_restful import Api
+from resources.emp import Emp
 
 app=Flask(__name__)
-# app.config['PROPAGATE_EXCEPTIONS']=True
-# app.config['PREFERRED_URL_SCHEME']='https'
-# api=Api(app)
-#
-# api.add_resource(Emp,'/emp')
-@app.route('/')
-def index():
-    return 'Hello heroku'
+app.config['PROPAGATE_EXCEPTIONS']=True
+app.config['PREFERRED_URL_SCHEME']='https'
+api=Api(app)
+
+api.add_resource(Emp,'/emp')
 
 if __name__ == "__main__":
     app.run()
