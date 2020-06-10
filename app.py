ad50bea6d0ee6317a0a35ace1c72efdc7ce1f731
preethu08052k <preethivardhanega@gmail.com>
2020-06-10 14:27:50 +0530

    heroku testing

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Api
 from resources.emp import Emp
 
 app=Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS']=True
+app.config['PREFERRED_URL_SCHEME']='https'
 api=Api(app)
 
 api.add_resource(Emp,'/emp')
