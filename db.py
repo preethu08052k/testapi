a9536a941702323e30aa9db7310986d909ea7ebf
preethu08052k <preethivardhanega@gmail.com>
2020-06-10 09:21:35 +0530

    using AWS MySQL DB

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ from decimal import Decimal
 import pymysql
 
 def query(querystr,return_json=True):
-    connection=pymysql.connect( host='localhost',
-                                user='root',
-                                password='',
+    connection=pymysql.connect( host='cosc-skillup.cxgok3weok8n.ap-south-1.rds.amazonaws.com',
+                                user='admin',
+                                password='coscskillup',
                                 db='testapi',
                                 cursorclass=pymysql.cursors.DictCursor )
     connection.begin()
