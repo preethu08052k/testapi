5bba1623c0a4f0b01d47ca81008c8b5c72e34a16
preethu08052k <preethivardhanega@gmail.com>
2020-06-10 15:03:09 +0530

    heroku testing

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from flask_restful import Api
-from waitress import serve
-from resources.emp import Emp
+# from flask_restful import Api
+# from resources.emp import Emp
 
 app=Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS']=True
-app.config['PREFERRED_URL_SCHEME']='https'
-api=Api(app)
+# app.config['PROPAGATE_EXCEPTIONS']=True
+# app.config['PREFERRED_URL_SCHEME']='https'
+# api=Api(app)
+#
+# api.add_resource(Emp,'/emp')
+@app.route('/')
+def index():
+    return 'Hello heroku'
 
-api.add_resource(Emp,'/emp')
-
-serve(app,host='127.0.0.1',port=5000,url_scheme='https')
+app.run()
