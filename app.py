43f5537eb2b8dc6e3ce84538f05faa187ee619d5
preethu08052k <preethivardhanega@gmail.com>
2020-06-10 13:50:57 +0530

    heroku testing

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
-from flask_restful import Api
-from resources.emp import Emp
-
-app=Flask(__name__)
-api=Api(app)
-
-api.add_resource(Emp,'/emp')
-
-app.run()
+# from flask import Flask
+# from flask_restful import Api
+# from resources.emp import Emp
+#
+# app=Flask(__name__)
+# api=Api(app)
+#
+# api.add_resource(Emp,'/emp')
+#
+# app.run()
+print('Hello world')
